0c5351cec6aa5f020a13bd36a5956afb5738d185
abdulvokhidjon <162565383+abdulvokhidjon@users.noreply.github.com>
2024-05-01 20:57:09 +0500

    mission completed

--- app.js
+++ app.js
@@ -47,3 +47,74 @@
 // let arr = [4, 5, 7, 8, 6, 9];
 // let output = alternateEnds(arr);
 // console.log(output.join(", "));
+
+// N{4}
+
+// function rangeSum(arr, K, L) {
+//     if (K < 0 || L >= arr.length || K > L) {
+//       return "Noto'g'ri kiritildi: K va L massiv chegaralarida bo'lishi kerak va K <= L";
+//     }
+
+//     return arr.slice(K, L + 1).reduce((sum, num) => sum + num, 0);
+//   }
+
+//   const arr = [1, 6, 9, 5, 8, 10, 15];
+//   const result = rangeSum(arr, 2, 5);
+//   console.log(result);
+
+// N{5}
+
+// function getSingleArr(arr) {
+//     let result = [];
+
+//     arr.forEach(element => {
+//         if (arr.indexOf(element) === arr.lastIndexOf(element)) {
+//             result.push(element);
+//         }
+//     });
+
+//     return result;
+// }
+
+// let arr = [7, 4, 2, 3, 1, 4, 5, 2, 4, 5, 7];
+// let output = getSingleArr(arr);
+// console.log(output);
+
+// N{6}
+
+// function swapMinMax(arr) {
+//   let minIndex = 0;
+//   let maxIndex = 0;
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < arr[minIndex]) minIndex = i;
+//     if (arr[i] > arr[maxIndex]) maxIndex = i;
+//   }
+
+//   [arr[minIndex], arr[maxIndex]] = [arr[maxIndex], arr[minIndex]];
+
+//   console.log("Max:", arr[maxIndex]);
+//   console.log("Min:", arr[minIndex]);
+//   return arr;
+// }
+
+// const arr = [7, 4, 9, 2, 3, 1, 5];
+// const result = swapMinMax(arr);
+// console.log(result);
+
+// N{7}
+
+// function cyclicShift(arr) {
+//   const lastElement = arr[arr.length - 1];
+
+//   for (let i = arr.length - 1; i > 0; i--) {
+//     arr[i] = arr[i - 1];
+//   }
+
+//   arr[0] = lastElement;
+
+//   return arr;
+// }
+
+// const arr = [1, 5, 8, 9, 10];
+// const result = cyclicShift(arr);
+// console.log(result);
